d2213b20f0cba1c3558d69ae653822236b2fa1a9
neo-anderson <hi.aswin@gmail.com>
2014-08-19 13:47:31 -0700

    Word count

--- edu.usc.pil.nlputils.application/src/edu/usc/pil/nlputils/application/parts/Welcome.java
+++ edu.usc.pil.nlputils.application/src/edu/usc/pil/nlputils/application/parts/Welcome.java
@@ -27,36 +27,21 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.layout.FillLayout;
 
 
 
 public class Welcome {
-
-	private Text txtInput;
 
 	@Inject
 	private MDirtyable dirty;
 
 	@PostConstruct
 	public void createComposite(Composite parent) {
-		parent.setLayout(new GridLayout(1, false));
-		
-		txtInput = new Text(parent, SWT.BORDER);
-		txtInput.setMessage("Enter text to mark part as dirty");
-		txtInput.addModifyListener(new ModifyListener() {
-			@Override
-			public void modifyText(ModifyEvent e) {
-				dirty.setDirty(true);
-			}
-		});
-		txtInput.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		parent.setLayout(new FillLayout(SWT.HORIZONTAL));
 		
 		Browser browser = new Browser(parent, SWT.NONE);
 		browser.setUrl("https://github.com/neo-anderson/NLPUtilsWiki/wiki/What's-New!");
-		GridData gd_browser = new GridData(SWT.CENTER, SWT.TOP, false, false, 1, 1);
-		gd_browser.widthHint = 500;
-		gd_browser.heightHint = 450;
-		browser.setLayoutData(gd_browser);
 	}
 
 	@Focus
