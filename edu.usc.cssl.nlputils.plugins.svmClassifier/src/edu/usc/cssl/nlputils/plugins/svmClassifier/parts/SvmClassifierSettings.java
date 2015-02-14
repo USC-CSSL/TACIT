f8e7f91f283987612eafd6293d88b31c34efc7b1
neo-anderson <hi.aswin@gmail.com>
2015-02-13 21:46:29 -0800

    #54 Tycho SVM fix

--- edu.usc.cssl.nlputils.plugins.svmClassifier/src/edu/usc/cssl/nlputils/plugins/svmClassifier/parts/SvmClassifierSettings.java
+++ edu.usc.cssl.nlputils.plugins.svmClassifier/src/edu/usc/cssl/nlputils/plugins/svmClassifier/parts/SvmClassifierSettings.java
@@ -200,7 +200,7 @@ public class SvmClassifierSettings {
 				//int selection = tabFolder.getSelectionIndex();
 				appendLog("PROCESSING...(SVM)");
 				// btnCrossVal.getSelection() always true
-				CrossValidator cv = new CrossValidator();
+				final CrossValidator cv = new CrossValidator();
 				ContextInjectionFactory.inject(cv,iEclipseContext);
 				
 				final String fppDir1 = ppDir1;
