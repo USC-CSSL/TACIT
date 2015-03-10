959fa67d98bdc0c001c9e71767bd0b4f1b74b33c
neo-anderson <hi.aswin@gmail.com>
2015-03-09 22:31:15 -0700

    #115 Asynchronous loading of data in the background implemented

--- edu.usc.cssl.nlputils.plugins.senatecrawler/src/edu/usc/cssl/nlputils/plugins/senatecrawler/parts/SenateCrawlerSettings.java
+++ edu.usc.cssl.nlputils.plugins.senatecrawler/src/edu/usc/cssl/nlputils/plugins/senatecrawler/parts/SenateCrawlerSettings.java
@@ -21,6 +21,7 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.Shell;
@@ -42,6 +43,8 @@ import org.osgi.framework.FrameworkUtil;
 public class SenateCrawlerSettings {
 	private Text txtMaxDocs;
 	private Text txtOutput;
+	String[] allSenators;
+	String[] congresses;
 	
 	@Inject
 	public SenateCrawlerSettings() {
@@ -54,31 +57,27 @@ public class SenateCrawlerSettings {
 	@PostConstruct
 	public void postConstruct(Composite parent) {
 		final Shell shell = parent.getShell();
-		appendLog("Loading Congresses...");
-		String[] congresses = null;
 		
-		try {
-			 congresses = AvailableRecords.getAllCongresses();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		appendLog("Loading Senators...");
-		String[] senatorsArray = null;
-
-		try {
-			senatorsArray = AvailableRecords.getAllSenators(congresses);
-		} catch (IOException e2) {
-			e2.printStackTrace();
-		}
-
-		final String[] allSenators = senatorsArray;
 		
-		appendLog("Loading Complete.");
+		
+		
+//		appendLog("Loading Senators...");
+//		String[] senatorsArray = null;
+//
+//		try {
+//			senatorsArray = AvailableRecords.getAllSenators(congresses);
+//		} catch (IOException e2) {
+//			e2.printStackTrace();
+//		}
+//
+//		final String[] allSenators = senatorsArray;
+		
+//		appendLog("Loading Complete.");
 		
 		Composite composite = new Composite(parent, SWT.NONE);
 		composite.setSize(588, 300);
 		composite.setLocation(0, 0);
+		
 		Label header = new Label(composite, SWT.NONE);
 		header.setImage(ImageDescriptor.createFromURL(
 				FileLocator.find(FrameworkUtil.getBundle(this.getClass()),
@@ -112,15 +111,17 @@ public class SenateCrawlerSettings {
 
 		final Combo cmbSenator = new Combo(composite, SWT.NONE);
 		cmbSenator.setBounds(89, 100, 300, 23);
-		cmbSenator.setItems(allSenators);
-		cmbSenator.add("All Senators", 0);
-		cmbSenator.add("All Democrats", 1);
-		cmbSenator.add("All Republicans", 2);
-		cmbSenator.add("All Independents", 3);
+//		cmbSenator.setItems(allSenators);
+//		cmbSenator.add("All Senators", 0);
+//		cmbSenator.add("All Democrats", 1);
+//		cmbSenator.add("All Republicans", 2);
+//		cmbSenator.add("All Independents", 3);
+		String[] loading = {"Loading..."};
+		cmbSenator.setItems(loading);
 		cmbSenator.select(0);
 		
 		final Combo cmbCongress = new Combo(composite, SWT.NONE);
-		cmbCongress.setBounds(89, 66, 60, 20);
+		cmbCongress.setBounds(89, 66, 101, 20);
 		cmbCongress.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -164,7 +165,7 @@ public class SenateCrawlerSettings {
 			}
 		});
 		//combo.setItems(new String[] {"All", "113"});
-		cmbCongress.setItems(congresses);
+		cmbCongress.setItems(loading);
 		cmbCongress.select(0);
 		
 		Label lblSenator_1 = new Label(composite, SWT.NONE);
@@ -283,6 +284,56 @@ public class SenateCrawlerSettings {
 		lblNewLabel.setText("\nYear\t\t\t\tCongress\r\n----------------------------------\r\n1989-1990\t-|-\t101st\r\n1991-1992\t-|-\t102nd\r\n1993-1994\t-|-\t103rd\r\n1995-1996\t-|-\t104th\r\n1997-1998\t-|-\t105th\r\n1999-2000\t-|-\t106th\r\n2001-2002\t-|-\t107th\r\n2003-2004\t-|-\t108th\r\n2005-2006\t-|-\t109th\r\n2007-2008\t-|-\t110th\r\n2009-2010\t-|-\t111th\r\n2011-2012\t-|-\t112th\r\n2013-2014\t-|-\t113th");
 		
 		
+		// Creating a new Job to fetch data for populating the drop down menus so that the UI will not freeze
+		Job job2 = new Job("Fetch Job"){
+			protected IStatus run(IProgressMonitor monitor){ 
+				
+				appendLog("Loading Congresses...");
+				String[] cgrss = null;
+				
+				try {
+					 cgrss = AvailableRecords.getAllCongresses();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				congresses = cgrss;
+				
+				Display.getDefault().asyncExec(new Runnable() {
+				      @Override
+				      public void run() {
+				    	cmbCongress.setItems(congresses);
+				  		cmbCongress.select(0);
+				      }});
+				
+				appendLog("Loading Senators...");
+				String[] senatorsArray = null;
+
+				try {
+					senatorsArray = AvailableRecords.getAllSenators(congresses);
+				} catch (IOException e2) {
+					e2.printStackTrace();
+				}
+
+				allSenators = senatorsArray;
+				// Async callback to access UI elements 
+				Display.getDefault().asyncExec(new Runnable() {
+				      @Override
+				      public void run() {	
+				    	cmbSenator.setItems(allSenators);
+				  		cmbSenator.add("All Senators", 0);
+				  		cmbSenator.add("All Democrats", 1);
+				  		cmbSenator.add("All Republicans", 2);
+				  		cmbSenator.add("All Independents", 3);
+				  		cmbSenator.select(0);
+				      }
+				});
+				
+				appendLog("Loading Complete.");
+			return Status.OK_STATUS;
+			}
+		};
+		job2.setUser(true);
+		job2.schedule();
 	}
 	
 	@Inject IEclipseContext context;
