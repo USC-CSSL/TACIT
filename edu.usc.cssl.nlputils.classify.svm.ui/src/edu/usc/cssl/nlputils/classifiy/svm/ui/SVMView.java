c3113da6239e2d65628ef20a6956ef79531fba43
anurag <anuragsi@usc.edu>
2015-05-26 16:36:00 -0700

    SVM implementation first commit

---
+++ edu.usc.cssl.nlputils.classify.svm.ui/src/edu/usc/cssl/nlputils/classifiy/svm/ui/SVMView.java
@@ -0,0 +1,38 @@
+package edu.usc.cssl.nlputils.classifiy.svm.ui;
+
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.part.ViewPart;
+
+import edu.usc.cssl.nlputils.classify.svm.ui.internal.ISVMViewConstants;
+
+public class SVMView extends ViewPart implements ISVMViewConstants {
+
+	private FormToolkit toolkit;
+	private ScrolledForm form;
+	public static String ID = "edu.usc.cssl.nlputils.classify.svm.ui.view";
+	@Override
+	public void createPartControl(Composite parent) {
+		toolkit = createFormBodySection(parent);
+		
+	}
+
+	private FormToolkit createFormBodySection(Composite parent) {
+		FormToolkit toolkit = new FormToolkit(parent.getDisplay());
+		form = toolkit.createScrolledForm(parent);
+
+		toolkit.decorateFormHeading(form.getForm());
+		form.setText("SVM Classifier"); //$NON-NLS-1$
+		GridLayoutFactory.fillDefaults().numColumns(1).equalWidth(true)
+				.applyTo(form.getBody());
+		return toolkit;
+	}
+	
+	@Override
+	public void setFocus() {
+		
+	}
+
+}
---
+++ edu.usc.cssl.nlputils.classify.svm.ui/src/edu/usc/cssl/nlputils/classify/svm/ui/handlers/OpenSVMViewHandler.java
@@ -0,0 +1,26 @@
+package edu.usc.cssl.nlputils.classify.svm.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import edu.usc.cssl.nlputils.classifiy.svm.ui.SVMView;
+
+public class OpenSVMViewHandler extends AbstractHandler{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		try {
+			HandlerUtil.getActiveWorkbenchWindowChecked(event).
+			getActivePage().showView(SVMView.ID);
+			
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+}
