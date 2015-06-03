f0b1c665d8783895e6ce4d2529dca4188093ca2b
anurag <anuragsi@usc.edu>
2015-06-03 15:45:17 -0700

    organized imports

--- edu.usc.cssl.nlputils.classify.svm.ui/src/edu/usc/cssl/nlputils/classifiy/svm/ui/SVMView.java
+++ edu.usc.cssl.nlputils.classify.svm.ui/src/edu/usc/cssl/nlputils/classifiy/svm/ui/SVMView.java
@@ -18,7 +18,6 @@ import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.PreferencesUtil;
