339646d996c252c17c482caf7e3db6be4a2c5544
vijayan <vijayan.balasubramanian@gmail.com>
2015-06-02 17:12:29 -0700

    Kmeans services
    added new service plug-in

---
+++ edu.usc.cssl.nlputils.cluster.kmeans/src/edu/uc/cssl/nlputils/cluster/kmeans/services/KmeansClusterAnalysis.java
@@ -0,0 +1,127 @@
+package edu.uc.cssl.nlputils.cluster.kmeans.services;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import weka.clusterers.SimpleKMeans;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+
+public class KmeansClusterAnalysis {
+	private static int[] doClustering(List<File> inputFiles, int numOfClusters) {
+		try {
+
+			StringToWordVector filter = new StringToWordVector();
+			SimpleKMeans kmeans = new SimpleKMeans();
+
+			FastVector atts = new FastVector(1);
+			atts.addElement(new Attribute("text", (FastVector) null));
+
+			Instances docs = new Instances("text_files", atts, 0);
+
+			for (int i = 0; i < inputFiles.size(); i++) {
+
+				try {
+					double[] newInst = new double[1];
+					String content = new Scanner(inputFiles.get(i))
+							.useDelimiter("\\Z").next();
+					newInst[0] = (double) docs.attribute(0).addStringValue(
+							content);
+					docs.add(new Instance(1.0, newInst));
+				} catch (Exception e) {
+					System.out.println("Exception occurred in reading files"
+							+ e);
+					return null;
+				}
+			}
+
+			filter.setInputFormat(docs);
+			Instances filteredData = Filter.useFilter(docs, filter);
+
+			kmeans.setPreserveInstancesOrder(true);
+			kmeans.setNumClusters(numOfClusters);
+			kmeans.buildClusterer(filteredData);
+			int[] assignments = kmeans.getAssignments();
+
+			int i = 0;
+			for (int clusterNum : assignments) {
+				System.out
+						.printf("Instance %d -> Cluster %d \n", i, clusterNum);
+				i++;
+			}
+
+			return assignments;
+		} catch (Exception e) {
+			System.out.println("Exception occurred in K means " + e);
+		}
+		return null;
+	}
+
+	public static void runClustering(int fNumClusters, File[] listOfFiles,
+			String fOutputDir) {
+
+		List<File> inputFiles = new ArrayList<File>();
+		for (File f : listOfFiles) {
+			if (f.getAbsolutePath().contains("DS_Store"))
+				continue;
+			inputFiles.add(f);
+		}
+
+		System.out.println("Running KMeans Clustering...");
+
+		int[] clusters = doClustering(inputFiles, fNumClusters);
+		if (clusters == null) {
+			System.out.println("Sorry. Something went wrong with KMeans Clustering. Please check your input and try again.\n");
+			return;
+		}
+		int i = 0;
+		System.out.println("Output for KMeans Clustering");
+		System.out.println("Clusters formed: \n");
+
+		Map<Integer, List<String>> outputClusters = new HashMap<Integer, List<String>>();
+		for (i = 0; i < fNumClusters; i++) {
+			outputClusters.put(i, new ArrayList<String>());
+		}
+
+		List<String> vec;
+		i = 0;
+		for (int clusterNum : clusters) {
+			vec = outputClusters.get(clusterNum);
+			vec.add(inputFiles.get(i).getName());
+			outputClusters.put(clusterNum, vec);
+			i++;
+		}
+
+		try {
+			String op = fOutputDir + File.separator + "KMeansClusters.txt";
+			System.out.println("Saving the output for Kmeans clustering in " + op);
+			FileWriter fw = new FileWriter(new File(op));
+			for (int c : outputClusters.keySet()) {
+				System.out.printf("Cluster %d \n", c);
+				System.out.println("Cluster " + c + ": \n");
+				fw.write("Cluster " + c + ": \n");
+				vec = outputClusters.get(c);
+				for (String f : vec) {
+					System.out.println("File " + f);
+					fw.write("File" + f + "\n");
+				}
+				fw.write("\n");
+				System.out.println("");
+			}
+			fw.close();
+		} catch (IOException e) {
+			System.out.println("Error writing output to files" + e);
+		}
+
+	}
+}
