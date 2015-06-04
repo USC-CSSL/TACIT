ff8f9c4f513dc5a16c5d82d465bfe1bc43060000
vijay <vijayanb@usc.edu>
2015-06-04 14:26:47 -0700

    added topic model

--- edu.usc.cssl.nlputils.repository/src/edu/usc/cssl/nlputils/repository/ApplicationWorkbenchWindowAdvisor.java
+++ edu.usc.cssl.nlputils.repository/src/edu/usc/cssl/nlputils/repository/ApplicationWorkbenchWindowAdvisor.java
@@ -1,6 +1,5 @@
 package edu.usc.cssl.nlputils.repository;
 
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
