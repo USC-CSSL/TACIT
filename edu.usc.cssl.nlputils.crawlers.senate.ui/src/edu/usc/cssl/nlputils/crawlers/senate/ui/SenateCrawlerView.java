2748a6b2bc70b0e3b60e4adc06e9439ce7440fc6
yuva29 <yuva.shankar29@gmail.com>
2015-06-15 11:41:16 -0700

    Senate Crawler

---
+++ edu.usc.cssl.nlputils.crawlers.senate.ui/src/edu/usc/cssl/nlputils/crawlers/senate/ui/SenateCrawlerView.java
@@ -0,0 +1,22 @@
+package edu.usc.cssl.nlputils.crawlers.senate.ui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class SenateCrawlerView extends ViewPart {
+
+	public static String ID = "edu.usc.cssl.nlputils.crawlers.senate.ui.senatecrawlerview";
+
+	@Override
+	public void createPartControl(Composite parent) {
+		
+		
+	}
+
+	@Override
+	public void setFocus() {
+		
+		
+	}
+
+}
---
+++ edu.usc.cssl.nlputils.crawlers.senate.ui/src/edu/usc/cssl/nlputils/crawlers/senate/ui/handlers/SenateCrawlerViewHandler.java
@@ -0,0 +1,25 @@
+package edu.usc.cssl.nlputils.crawlers.senate.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import edu.usc.cssl.nlputils.crawlers.senate.ui.SenateCrawlerView;
+
+public class SenateCrawlerViewHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		try {
+			HandlerUtil.getActiveWorkbenchWindowChecked(event).getActivePage()
+					.showView(SenateCrawlerView.ID);
+
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
