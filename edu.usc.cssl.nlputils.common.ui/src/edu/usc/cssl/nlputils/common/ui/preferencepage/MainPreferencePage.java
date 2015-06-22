0329c58d467e938cdaf824810838b9ec03f96b5d
anurag <anuragsi@usc.edu>
2015-06-22 12:30:40 -0700

    add checkbox in preferences to create readme file.

--- edu.usc.cssl.nlputils.common.ui/src/edu/usc/cssl/nlputils/common/ui/preferencepage/MainPreferencePage.java
+++ edu.usc.cssl.nlputils.common.ui/src/edu/usc/cssl/nlputils/common/ui/preferencepage/MainPreferencePage.java
@@ -1,15 +1,24 @@
 package edu.usc.cssl.nlputils.common.ui.preferencepage;
 
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
-public class MainPreferencePage extends PreferencePage implements
-		IWorkbenchPreferencePage {
+import edu.usc.cssl.nlputils.common.ui.CommonUiActivator;
+import edu.usc.cssl.nlputils.common.ui.ICommonUiConstants;
 
+public class MainPreferencePage extends PreferencePage implements
+		IWorkbenchPreferencePage,ICommonUiConstants {
+
+	private Button readMe;
 	public MainPreferencePage() {
 		// TODO Auto-generated constructor stub
 	}
@@ -26,14 +35,83 @@ public class MainPreferencePage extends PreferencePage implements
 
 	@Override
 	public void init(IWorkbench workbench) {
-		// TODO Auto-generated method stub
+		setPreferenceStore(CommonUiActivator.getDefault().getPreferenceStore());
 
 	}
 
 	@Override
 	protected Control createContents(Composite parent) {
-		// TODO Auto-generated method stub
-		return null;
+		Composite sectionClient = new Composite(parent, SWT.NONE);
+		GridLayoutFactory.fillDefaults().numColumns(3).equalWidth(false)
+				.applyTo(sectionClient);
+
+		Label dummy = new Label(sectionClient, SWT.NONE);
+		GridDataFactory.fillDefaults().grab(false, false).span(3, 0)
+				.applyTo(dummy);
+		
+		readMe = createReadMeSection(sectionClient);
+		
+		initializeDefaultValues();
+		loadValues();
+		if (Boolean.valueOf(load(INITIAL))) {
+			performDefaults();
+		}
+		
+		return sectionClient;
+	}
+	
+	@Override
+	protected void performApply() {
+		performOk();
+		super.performApply();
+	}
+
+	@Override
+	protected void performDefaults() {
+		setDefaultValues();
+		super.performDefaults();
+	}
+	
+	private void initializeDefaultValues(){
+		getPreferenceStore().setDefault(INITIAL, "true");
+		getPreferenceStore().setDefault(CREATE_README, "true");
+	}
+	
+	private void setDefaultValues() {
+		readMe.setSelection(Boolean.valueOf(getPreferenceStore().getDefaultString(CREATE_README)));
+	}
+	
+	private void loadValues(){
+		readMe.setSelection(Boolean.valueOf(load(CREATE_README)));
+	}
+	
+	private String load(String name) {
+		return getPreferenceStore().getString(name);
+	}
+	
+	
+	private Button createReadMeSection(Composite sectionClient) {
+
+		final Button readMe = new Button(sectionClient, SWT.CHECK);
+		GridDataFactory.fillDefaults().grab(false, false).span(1, 0)
+				.applyTo(readMe);
+		readMe.setText("Generate README file");
+		readMe.setSelection(true);
+
+		return readMe;
+	}
+	
+	@Override
+	public boolean performOk() {
+
+		store(INITIAL, Boolean.toString(false));
+		store(CREATE_README, Boolean.toString(readMe.getSelection()));
+		return super.performOk();
+	}
+
+	private void store(String name, String value) {
+		getPreferenceStore().setValue(name, value);
+
 	}
 
 }
