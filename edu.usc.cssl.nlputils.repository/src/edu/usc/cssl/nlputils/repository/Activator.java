ba2ac7f478fcdb83d6f08c1dcef5cb160d68e867
anurag <anuragsi@usc.edu>
2015-06-23 12:50:07 -0700

    Fix plugin id

--- edu.usc.cssl.nlputils.repository/src/edu/usc/cssl/nlputils/repository/Activator.java
+++ edu.usc.cssl.nlputils.repository/src/edu/usc/cssl/nlputils/repository/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "usc.edu.cssl.nlputils.product"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "edu.usc.cssl.nlputils.repository"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
