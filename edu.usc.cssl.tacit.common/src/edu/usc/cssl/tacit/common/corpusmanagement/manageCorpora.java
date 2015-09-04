40d22db18c754c161b6f6089cbac2f3b441a94a8
anurag <anuragsi@usc.edu>
2015-09-03 18:34:11 -0700

    update CM code

--- edu.usc.cssl.tacit.common/src/edu/usc/cssl/tacit/common/corpusmanagement/manageCorpora.java
+++ edu.usc.cssl.tacit.common/src/edu/usc/cssl/tacit/common/corpusmanagement/manageCorpora.java
@@ -32,63 +32,91 @@ public class manageCorpora {
 						+ System.getProperty("file.separator") + "meta.txt";
 
 				File metaFp = new File(metaFile);
-				
+
 				JSONObject jsonObj = new JSONObject();
-				jsonObj.put("corpus_name","corpusID");
+				jsonObj.put("corpus_name", corpus.getCorpusId());
 				jsonObj.put("data_type", corpus.getDatatype());
-				jsonObj.put("num_classes",Integer.toString(corpus.getClasses().size()));
-				
+				jsonObj.put("num_classes",
+						Integer.toString(corpus.getClasses().size()));
+
 				int numClasses = corpus.getClasses().size();
-				ArrayList<ICorpusClass> corporaClasses = (ArrayList<ICorpusClass>) corpus.getClasses();
-				
-				JSONArray classArray = new JSONArray(); 
-				
-				for (int i=0; i<numClasses; i++) {
+				ArrayList<ICorpusClass> corporaClasses = (ArrayList<ICorpusClass>) corpus
+						.getClasses();
+
+				JSONArray classArray = new JSONArray();
+
+				for (int i = 0; i < numClasses; i++) {
 					CorpusClass currClass = (CorpusClass) corporaClasses.get(i);
 					JSONObject classObj = new JSONObject();
 					classObj.put("class_name", currClass.getClassName());
 					classObj.put("original_loc", currClass.getClassPath());
-					
-					String[] dirParts = currClass.getClassName().split(System.getProperty("file.separator"));
-					String dirName = dirParts[dirParts.length-1];
-					classObj.put("tacit_loc", corpusLocation+System.getProperty("file.separator")+dirName);
-					
+
+					String[] dirParts = currClass.getClassName().split(
+							System.getProperty("file.separator"));
+					String dirName = dirParts[dirParts.length - 1];
+					classObj.put(
+							"tacit_loc",
+							corpusLocation
+									+ System.getProperty("file.separator")
+									+ dirName);
+
 					classArray.add(classObj);
 				}
-				
+
 				jsonObj.put("class_details", classArray);
 				jsonObj.put("num_analysis", "0");
 				JSONArray analysisArray = new JSONArray();
 				jsonObj.put("prev_analysis", analysisArray);
-				
+
 				try {
-					BufferedWriter bw = new BufferedWriter(new FileWriter(metaFp));
+					BufferedWriter bw = new BufferedWriter(new FileWriter(
+							metaFp));
 					bw.write(jsonObj.toString());
 					bw.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-				
+
 				copyCorpus(jsonObj);
-				
+
 			} else {
 				// corpus already exits, check what has changed
+
+				String metaFile = corpusLocation
+						+ System.getProperty("file.separator") + "meta.txt";
+
+				File metaFp = new File(metaFile);
+
+				JSONObject jsonObj = new JSONObject();
+				jsonObj.put("corpus_name", corpus.getCorpusId());
+				jsonObj.put("data_type", corpus.getDatatype());
+				jsonObj.put("num_classes",
+						Integer.toString(corpus.getClasses().size()));
+
+				int numClasses = corpus.getClasses().size();
+				ArrayList<ICorpusClass> corporaClasses = (ArrayList<ICorpusClass>) corpus
+						.getClasses();
+
+				JSONArray classArray = new JSONArray();
 			}
 		}
 	}
-	
+
 	private static void copyCorpus(JSONObject jsonObj) {
 		int numClasses = (Integer) jsonObj.get("num_classes");
 		JSONArray classArray = (JSONArray) jsonObj.get("class_details");
-		
-		for (int i=0; i<numClasses; i++) {
-			String originalLoc = (String) ((JSONObject) classArray.get(i)).get("original_loc");
-			String tacitLoc = (String) ((JSONObject) classArray.get(i)).get("tacit_loc");
-			
+
+		for (int i = 0; i < numClasses; i++) {
+			String originalLoc = (String) ((JSONObject) classArray.get(i))
+					.get("original_loc");
+			String tacitLoc = (String) ((JSONObject) classArray.get(i))
+					.get("tacit_loc");
+
 			new File(tacitLoc).mkdir();
-			
+
 			try {
-				FileUtils.copyDirectory(new File(originalLoc), new File(tacitLoc));
+				FileUtils.copyDirectory(new File(originalLoc), new File(
+						tacitLoc));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
