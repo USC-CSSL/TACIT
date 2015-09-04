1f7bd216ca6967059c88adac3e133e5c2b1c5257
lindapulickal <pulickal@usc.edu>
2015-09-03 20:27:52 -0700

    changed SaveCorpora to atomic unit

--- edu.usc.cssl.tacit.common/src/edu/usc/cssl/tacit/common/corpusmanagement/manageCorpora.java
+++ edu.usc.cssl.tacit.common/src/edu/usc/cssl/tacit/common/corpusmanagement/manageCorpora.java
@@ -21,85 +21,91 @@ public class manageCorpora {
 			+ System.getProperty("file.separator");
 
 	@SuppressWarnings("unchecked")
-	public static void saveCorpus(ArrayList<Corpus> corporaList) {
+	public static void saveCorpuses(ArrayList<Corpus> corporaList) {
 		for (Corpus corpus : corporaList) {
-			String corpusID = corpus.getCorpusId();
-			String corpusLocation = rootDir + corpusID;
-			if (!(new File(corpusLocation).exists())) {
-				// Add code for everything
-				new File(corpusLocation).mkdir();
-				String metaFile = corpusLocation
-						+ System.getProperty("file.separator") + "meta.txt";
-
-				File metaFp = new File(metaFile);
-
-				JSONObject jsonObj = new JSONObject();
-				jsonObj.put("corpus_name", corpus.getCorpusId());
-				jsonObj.put("data_type", corpus.getDatatype());
-				jsonObj.put("num_classes",
-						Integer.toString(corpus.getClasses().size()));
-
-				int numClasses = corpus.getClasses().size();
-				ArrayList<ICorpusClass> corporaClasses = (ArrayList<ICorpusClass>) corpus
-						.getClasses();
-
-				JSONArray classArray = new JSONArray();
-
-				for (int i = 0; i < numClasses; i++) {
-					CorpusClass currClass = (CorpusClass) corporaClasses.get(i);
-					JSONObject classObj = new JSONObject();
-					classObj.put("class_name", currClass.getClassName());
-					classObj.put("original_loc", currClass.getClassPath());
-
-					String[] dirParts = currClass.getClassName().split(
-							System.getProperty("file.separator"));
-					String dirName = dirParts[dirParts.length - 1];
-					classObj.put(
-							"tacit_loc",
-							corpusLocation
-									+ System.getProperty("file.separator")
-									+ dirName);
-
-					classArray.add(classObj);
-				}
-
-				jsonObj.put("class_details", classArray);
-				jsonObj.put("num_analysis", "0");
-				JSONArray analysisArray = new JSONArray();
-				jsonObj.put("prev_analysis", analysisArray);
-
-				try {
-					BufferedWriter bw = new BufferedWriter(new FileWriter(
-							metaFp));
-					bw.write(jsonObj.toString());
-					bw.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-				copyCorpus(jsonObj);
-
-			} else {
-				// corpus already exits, check what has changed
-
-				String metaFile = corpusLocation
-						+ System.getProperty("file.separator") + "meta.txt";
-
-				File metaFp = new File(metaFile);
-
-				JSONObject jsonObj = new JSONObject();
-				jsonObj.put("corpus_name", corpus.getCorpusId());
-				jsonObj.put("data_type", corpus.getDatatype());
-				jsonObj.put("num_classes",
-						Integer.toString(corpus.getClasses().size()));
-
-				int numClasses = corpus.getClasses().size();
-				ArrayList<ICorpusClass> corporaClasses = (ArrayList<ICorpusClass>) corpus
-						.getClasses();
-
-				JSONArray classArray = new JSONArray();
-			}
+			saveCorpus(corpus);
 		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void saveCorpus(Corpus corpus) {
+		String corpusID = corpus.getCorpusId();
+		String corpusLocation = rootDir + corpusID;
+		if (!(new File(corpusLocation).exists())) {
+			// Add code for everything
+			new File(corpusLocation).mkdir();
+			String metaFile = corpusLocation
+					+ System.getProperty("file.separator") + "meta.txt";
+
+			File metaFp = new File(metaFile);
+
+			JSONObject jsonObj = new JSONObject();
+			jsonObj.put("corpus_name", corpus.getCorpusId());
+			jsonObj.put("data_type", corpus.getDatatype());
+			jsonObj.put("num_classes",
+					Integer.toString(corpus.getClasses().size()));
+
+			int numClasses = corpus.getClasses().size();
+			ArrayList<ICorpusClass> corporaClasses = (ArrayList<ICorpusClass>) corpus
+					.getClasses();
+
+			JSONArray classArray = new JSONArray();
+
+			for (int i = 0; i < numClasses; i++) {
+				CorpusClass currClass = (CorpusClass) corporaClasses.get(i);
+				JSONObject classObj = new JSONObject();
+				classObj.put("class_name", currClass.getClassName());
+				classObj.put("original_loc", currClass.getClassPath());
+
+				String[] dirParts = currClass.getClassName().split(
+						System.getProperty("file.separator"));
+				String dirName = dirParts[dirParts.length - 1];
+				classObj.put(
+						"tacit_loc",
+						corpusLocation
+								+ System.getProperty("file.separator")
+								+ dirName);
+
+				classArray.add(classObj);
+			}
+
+			jsonObj.put("class_details", classArray);
+			jsonObj.put("num_analysis", "0");
+			JSONArray analysisArray = new JSONArray();
+			jsonObj.put("prev_analysis", analysisArray);
+
+			try {
+				BufferedWriter bw = new BufferedWriter(new FileWriter(
+						metaFp));
+				bw.write(jsonObj.toString());
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			copyCorpus(jsonObj);
+
+		} else {
+			// corpus already exits, check what has changed
+
+			String metaFile = corpusLocation
+					+ System.getProperty("file.separator") + "meta.txt";
+
+			File metaFp = new File(metaFile);
+
+			JSONObject jsonObj = new JSONObject();
+			jsonObj.put("corpus_name", corpus.getCorpusId());
+			jsonObj.put("data_type", corpus.getDatatype());
+			jsonObj.put("num_classes",
+					Integer.toString(corpus.getClasses().size()));
+
+			int numClasses = corpus.getClasses().size();
+			ArrayList<ICorpusClass> corporaClasses = (ArrayList<ICorpusClass>) corpus
+					.getClasses();
+
+			JSONArray classArray = new JSONArray();
+		}
+	
 	}
 
 	private static void copyCorpus(JSONObject jsonObj) {
