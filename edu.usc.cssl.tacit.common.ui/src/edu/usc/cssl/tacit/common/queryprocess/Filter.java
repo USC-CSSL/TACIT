fcb0c4322d4d6ae3ed796f167ff18aa1c2ab8fce
lindapulickal <pulickal@usc.edu>
2015-11-24 11:19:57 -0800

    added filter description method

--- edu.usc.cssl.tacit.common.ui/src/edu/usc/cssl/tacit/common/queryprocess/Filter.java
+++ edu.usc.cssl.tacit.common.ui/src/edu/usc/cssl/tacit/common/queryprocess/Filter.java
@@ -5,7 +5,7 @@ public class Filter {
 	QueryOperatorType operator = null;
 	String filterValue = null;
 	QueryDataType targetType = null;
-	
+
 	public QueryDataType getTargetType() {
 		return targetType;
 	}
@@ -16,29 +16,39 @@ public class Filter {
 
 	public Filter() {
 	}
-	
-	public Filter(String targetName, QueryOperatorType operator, String filterValue, QueryDataType targetType) {
+
+	public Filter(String targetName, QueryOperatorType operator,
+			String filterValue, QueryDataType targetType) {
 		this.targetName = targetName;
 		this.operator = operator;
 		this.filterValue = filterValue;
 		this.targetType = targetType;
 	}
-	
+
+	public String getDescription() {
+		return targetName + " ; " + operator.toString() + " ; " + filterValue;
+	}
+
 	public String getTargetName() {
 		return targetName;
 	}
+
 	public void setTargetType(String targetName) {
 		this.targetName = targetName;
 	}
+
 	public QueryOperatorType getOperationType() {
 		return operator;
 	}
+
 	public void setOperationType(QueryOperatorType operationType) {
 		this.operator = operationType;
 	}
+
 	public String getFilterValue() {
 		return filterValue;
 	}
+
 	public void setFilterValue(String filterValue) {
 		this.filterValue = filterValue;
 	}
