408c3f6a528c167cdbb0db2993755b7e8d32e1ee
dnishant <dnishant93@gmail.com>
2016-05-23 11:23:41 -0700

    Test Wiki Crawler Commit

---
+++ edu.usc.cssl.tacit.crawlers.wikipedia.ui/src/edu/usc/cssl/tacit/crawlers/wikipedia/ui/internal/IWikipediaCrawlerUIConstants.java
@@ -0,0 +1,10 @@
+package edu.usc.cssl.tacit.crawlers.wikipedia.ui.internal;
+
+public interface IWikipediaCrawlerUIConstants {
+	public static final String IMAGE_LRUN_OBJ = "lrun_obj";
+	public static final String IMAGE_HELP_CO = "help_co";
+	public static final String IMAGE_CRAWL = "crawl";
+	public static final String IMAGE_STACK_OBJ = "stackexchange";
+	String DEFAULT_CORPUS_LOCATION = System.getProperty("user.dir") + System.getProperty("file.separator") + "json_corpuses" + System.getProperty("file.separator") + "wikipedia";
+
+}
