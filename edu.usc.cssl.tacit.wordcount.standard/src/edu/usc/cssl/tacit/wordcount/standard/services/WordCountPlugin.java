6908f7eddfbdb8c63b79b77b45dfb32b0a25e6a8
dnishant <dnishant93@gmail.com>
2016-05-25 18:01:48 -0700

    #459

--- edu.usc.cssl.tacit.wordcount.standard/src/edu/usc/cssl/tacit/wordcount/standard/services/WordCountPlugin.java
+++ edu.usc.cssl.tacit.wordcount.standard/src/edu/usc/cssl/tacit/wordcount/standard/services/WordCountPlugin.java
@@ -793,7 +793,7 @@ public class WordCountPlugin {
 					 */
 					for (int i = 1; i < words.length; i = increment(i)) {
 						
-						if (words[i].contains(")") || words[i].contains("("))
+						if (words[i].contains(")") || words[i].contains("(") || words[i].contains("<"))
 							continue;
 						// Add a category to the maps if it was not added
 						// earlier
