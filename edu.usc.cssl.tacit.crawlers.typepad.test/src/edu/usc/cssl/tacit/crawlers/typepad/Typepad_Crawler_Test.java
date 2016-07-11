e795f8e46fab5f66cedd56b4d88b7be319802483
Sagar Makwana <makwana_sagar@outlook.com>
2016-07-11 12:33:45 -0700

    TypePad Crawler JUnit Test cases added

---
+++ edu.usc.cssl.tacit.crawlers.typepad.test/src/edu/usc/cssl/tacit/crawlers/typepad/Typepad_Crawler_Test.java
@@ -0,0 +1,61 @@
+package edu.usc.cssl.tacit.crawlers.typepad;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.Test;
+
+import edu.usc.cssl.tacit.crawlers.typepad.services.TypePadCrawler;
+
+public class Typepad_Crawler_Test {
+	final File testDirectory = new File("TestData");
+	Exception exceptionObj = null;
+	
+	@Test
+	public void testGenericCrawl() {
+		TypePadCrawler typePadCrawler = new TypePadCrawler();
+		ArrayList<String> genericKeywords = new ArrayList<String>();
+		genericKeywords.add("\"star wars\"");
+		genericKeywords.add("galaxy");
+		if (!testDirectory.exists()){
+			testDirectory.mkdir();
+		}
+			
+		try{
+			typePadCrawler.getQueryResults(genericKeywords, null, null, 100, 1, testDirectory.getAbsolutePath(), "GenericTestData", new NullProgressMonitor());	
+			FileUtils.deleteDirectory(testDirectory);
+		}
+		catch(Exception e){
+			exceptionObj = e;
+		}
+		
+		assertEquals("Checking if any exception occured in generic typepad crawl.", exceptionObj, null);
+	}
+	
+	@Test
+	public void testConentAndTitleCrawl() {
+		TypePadCrawler typePadCrawler = new TypePadCrawler();
+		ArrayList<String> titleKeywords = new ArrayList<String>();
+		titleKeywords.add("\"star wars\"");
+		ArrayList<String> contentKeywords = new ArrayList<String>();
+		contentKeywords.add("galaxy");
+		
+		if (!testDirectory.exists()){
+			testDirectory.mkdir();
+		}
+			
+		try{
+			typePadCrawler.getQueryResults(null, contentKeywords, titleKeywords, 100, 1, testDirectory.getAbsolutePath(), "ContentTitleTestData", new NullProgressMonitor());	
+			FileUtils.deleteDirectory(testDirectory);
+		}
+		catch(Exception e){
+			exceptionObj = e;
+		}
+		
+		assertEquals("Checking if any exception occured in generic typepad crawl.", exceptionObj, null);
+	}
+}
