5cfb5896fb0d8a84b5f445b51fe7574800eb097b
dnishant <dnishant93@gmail.com>
2017-01-05 10:51:17 -0800

    Dict in .txt format-- minor change

--- edu.usc.cssl.tacit.wordcount.standard.ui/src/edu/usc/cssl/tacit/wordcount/standard/ui/StandardWordCountView.java
+++ edu.usc.cssl.tacit.wordcount.standard.ui/src/edu/usc/cssl/tacit/wordcount/standard/ui/StandardWordCountView.java
@@ -458,19 +458,19 @@ public class StandardWordCountView extends ViewPart implements
 			return false;
 		}
 		
-		try{
-			List<Object> dictPaths = dictLayoutData.getTypeCheckedSelectedFiles(checkType);
-			Iterator<Object> dictPathIterator  = dictPaths.iterator();
-			while(dictPathIterator.hasNext()){
-				String dictPath  = (String)dictPathIterator.next();
-				if (!dictPath.endsWith(".txt")){
-					form.getMessageManager().addMessage("dict", "Error:The dictionary is not in .txt format", null, IMessageProvider.ERROR);
-					return false;
-				}
-			}
-		}catch(Exception e){
-			return false;
-		}
+//		try{
+//			List<Object> dictPaths = dictLayoutData.getTypeCheckedSelectedFiles(checkType);
+//			Iterator<Object> dictPathIterator  = dictPaths.iterator();
+//			while(dictPathIterator.hasNext()){
+//				String dictPath  = (String)dictPathIterator.next();
+//				if (!dictPath.endsWith(".txt")){
+//					form.getMessageManager().addMessage("dict", "Error:The dictionary is not in .txt format", null, IMessageProvider.ERROR);
+//					return false;
+//				}
+//			}
+//		}catch(Exception e){
+//			return false;
+//		}
 		
 		return true;
 	}
